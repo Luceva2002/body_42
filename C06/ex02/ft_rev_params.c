446fbc415c389e117ba86b9c4577f394a782d89c
luceva2002 <luceva2002@gmail.com>
2024-10-10 02:03:55 +0200

    giggi

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_rev_params(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		ft_print_params(av[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
