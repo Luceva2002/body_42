7dcdca09e09def5827b1057a79b8daf6ae462fb2
Luca Evangelista <luevange@e3r5p3.42roma.it>
2024-10-10 11:43:52 +0200

    ciccio

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luevange <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 10:57:29 by luevange          #+#    #+#             */
+/*   Updated: 2024/10/10 10:58:24 by luevange         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_rev_params(char *str)
@@ -14,13 +26,13 @@ void	ft_rev_params(char *str)
 
 int	main(int ac, char **av)
 {
-	int i;
+	int	i;
 
 	i = ac - 1;
 	while (i != 0)
 	{
 		ft_rev_params(av[i]);
-		write(1, "\n", 1);
+		write (1, "\n", 1);
 		i--;
 	}
 	return (0);
